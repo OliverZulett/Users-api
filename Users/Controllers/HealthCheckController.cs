2fda168f2f17efbaf89c61309bfd5e89edb0c1c3
oliver zulett <oliverzulett@gmail.com>
2023-05-29 20:59:39 -0400

    add check health contoller

---
+++ Users/Controllers/HealthCheckController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Users.Models.Data;
+
+namespace Users.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly UsersContext _context;
+        private readonly ILogger<HealthCheckController> _logger;
+
+        public HealthCheckController(UsersContext context, ILogger<HealthCheckController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ActionName(nameof(CheckHealth))]
+        public IActionResult CheckHealth() {
+            bool isDatabaseHealthy = CheckDatabaseHealth();
+            bool areExternalServicesHealthy = CheckExternalServicesHealth();
+            bool isApiHealthy = isDatabaseHealthy && areExternalServicesHealthy;
+            if (isApiHealthy)
+            {
+                _logger.LogInformation("API health check successful.");
+                return Ok("API is healthy");
+            }
+            else
+            {
+                return StatusCode(503, "API is not healthy");
+            }
+        }
+
+        private bool CheckDatabaseHealth()
+        {
+            bool isDatabaseHealthy = false;
+
+            try
+            {
+                using (_context)
+                {
+                    if (_context.Database.CanConnect())
+                    {
+                        // The database connection is successful
+                        _logger.LogInformation("The database connection is successful.");
+                        isDatabaseHealthy = true;
+                    }
+                    else
+                    {
+                        // The database connection is not successful
+                        _logger.LogInformation("Failed to establish a connection to the database.");
+                        isDatabaseHealthy = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while checking the database health.");
+                isDatabaseHealthy = false;
+            }
+
+            return isDatabaseHealthy;
+        }
+
+        private bool CheckExternalServicesHealth()
+        {
+            return true;
+        }
+    }
+}
---
+++ UsersTests/Controllers/HealthCheckControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Users.Controllers;
+using Users.Models.Data;
+using Users.Models.Repository;
+using Xunit;
+
+namespace UsersTests.Controllers
+{
+    public class HealthCheckControllerTests
+    {
+        private readonly HealthCheckController _healthCheckController;
+
+        public HealthCheckControllerTests()
+        {
+            // Configurar la base de datos en memoria
+            var options = new DbContextOptionsBuilder<UsersContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            // Crear una instancia del contexto utilizando la base de datos en memoria
+            var context = new UsersContext(options);
+
+            // Crear una instancia del logger utilizando un LoggerFactory
+            var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+            var logger = loggerFactory.CreateLogger<HealthCheckController>();
+
+            // Crear una instancia del controlador con el contexto y el logger
+            _healthCheckController = new HealthCheckController(context, logger);
+        }
+
+        [Fact]
+        public void CheckHealth_ReturnsOkWhenApiIsHealthy()
+        {
+            // Act
+            var result = _healthCheckController.CheckHealth();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("API is healthy", ((OkObjectResult)result).Value);
+        }
+    }
+}
